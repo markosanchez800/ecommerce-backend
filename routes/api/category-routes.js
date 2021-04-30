89d833a0887677a53214ad6cf38a58f05216f966
markosanchez800 <markosanchez800@gmail.com>
2021-04-29 20:56:30 -0700

    can now delete a category using delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -56,8 +56,19 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const catData = await Category.destroy({
+      where:{id:req.params.id}
+    });
+    if(!catData){
+      res.status(404).json({message:"No category found matching your selection, try again"});
+    }
+    res.status(200).json(catData);
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
