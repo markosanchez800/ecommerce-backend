cef0790a96d1d2702bac7ccda13e9ac978209cd7
markosanchez800 <markosanchez800@gmail.com>
2021-04-29 20:36:36 -0700

    can now create new categories with post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,8 +36,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const catData = await Category.create(req.body);
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
