cf924f60a4065403010ebad4bf6a7cca9365c09c
markosanchez800 <markosanchez800@gmail.com>
2021-04-29 18:52:13 -0700

    can now properly delete products from database

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,7 +34,7 @@ try {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -107,8 +107,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+ try{
+   const productData = await Product.destroy({
+     where:{id:req.params.id}
+   });
+   if(!productData){
+     res.status(404).json({message:"No product found matching this ID, try again"});
+   }
+   res.status(200).json(productData);
+ } catch (err){
+   res.status(500).json(err);
+ }
 });
 
 module.exports = router;
