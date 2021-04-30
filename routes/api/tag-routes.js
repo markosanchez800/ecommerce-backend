1335b406fc0fa08521e2885a502b3d22b9a5eebc
markosanchez800 <markosanchez800@gmail.com>
2021-04-29 19:47:45 -0700

    get tag by id route working properly with verification check

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,9 +17,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+ try {
+   const tagData = await Tag.findByPk(req.params.id,{
+     include:[
+       Product,{model: Product,through: ProductTag}
+     ],
+   });
+   if(!tagData){
+     res.status(404).json({message:"No ID matching your selection, try again"});
+   }
+   console.log(tagData);
+   res.status(200).json(tagData);
+ } catch (err) {
+   res.status(500).json(err);
+ }
 });
 
 router.post('/', (req, res) => {
