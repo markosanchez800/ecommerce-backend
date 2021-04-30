0d3a057ea680a01afc34855d0f1ad91d7389bdaf
markosanchez800 <markosanchez800@gmail.com>
2021-04-29 19:02:08 -0700

    get all tags route works properly

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,6 +5,6 @@ const tagRoutes = require('./tag-routes');
 
 //router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
-//router.use('/tags', tagRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;
