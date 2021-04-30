9a6f31c188434f492237f73a47cadf5b63bc5fe3
markosanchez800 <markosanchez800@gmail.com>
2021-04-29 18:23:03 -0700

    get product by id route now working properly

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,8 +5,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
       include: [
@@ -21,9 +19,18 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+try {
+  const productData = await Product.findByPk(req.params.id,{
+    include:[
+      Category,{model: Tag,through: ProductTag}
+    ],
+  });
+  console.log(productData);
+  res.status(200).json(productData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 // create new product
