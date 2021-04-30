09aa1afc1423b8135bbbd0f50269b63eeda47893
markosanchez800 <markosanchez800@gmail.com>
2021-04-29 20:13:28 -0700

    can now delete tags with delete route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,8 +52,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+ try{
+   const tagData = await Tag.destroy({
+     where:{id:req.params.id}
+   });
+   if(!tagData){
+     res.status(404).json({message:"No tag found matching that ID, try again"});
+   }
+   res.status(200).json(tagData);
+ } catch (err){
+   res.status(500).json(err);
+ }
 });
 
 module.exports = router;
